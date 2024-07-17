6a86abf2236bdb9103d1773619a2f8d8df3bd876
Mustafa Talha <mtilerisoy@gmail.com>
2024-07-17 15:25:47 +0200

    encapsulate main function

--- scripts/crop_humans.py
+++ scripts/crop_humans.py
@@ -4,18 +4,22 @@ from PIL import Image
 import numpy as np
 import os
 
+def crop_and_save_images(image_path, output_dir="cropped_images", model_path='yolov10x.pt'):
+    """
+    Crops images detected by YOLO model and saves them to a specified directory.
 
-if __name__ == "__main__":
-
-    # Ensure the 'cropped_images' directory exists
-    output_dir = "cropped_images"
+    Parameters:
+    - image_path: Path to the input image.
+    - output_dir: Directory where cropped images will be saved.
+    - model_path: Path to the YOLO model.
+    """
+    # Ensure the output directory exists
     os.makedirs(output_dir, exist_ok=True)
 
-    # Load the YOLOv8 model with the updated 'u' model for improved performance
-    model = YOLO('yolov10x.pt')  # Updated model path
+    # Load the YOLO model
+    model = YOLO(model_path)
 
     # Load an image
-    image_path = 'data/model/x.jpg'
     image = Image.open(image_path)
 
     # Resize the image to make its dimensions divisible by 32
@@ -34,7 +38,6 @@ if __name__ == "__main__":
     classes = results[0].boxes.cls.cpu().tolist()
     
     for box, cls in zip(boxes, classes):
-        
         # Get the class name from the model class dictionary
         class_name = model.names[cls]
 
@@ -45,3 +48,8 @@ if __name__ == "__main__":
         crop_img.save(os.path.join(output_dir, f"{class_name}_{int(box[0])}_{int(box[1])}.jpg"))
     
     print("Cropping and saving complete.")
+
+if __name__ == "__main__":
+    
+    image_path = 'data/model/x.jpg'
+    crop_and_save_images(image_path)
