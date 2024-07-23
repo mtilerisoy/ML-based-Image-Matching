ffe573c1cc1a5595781730bb4d2c04f0e3f66b9a
Mustafa Talha <mtilerisoy@gmail.com>
2024-07-23 10:15:33 +0200

    return multiple croped images

--- scripts/crop_humans.py
+++ scripts/crop_humans.py
@@ -32,6 +32,8 @@ def crop_image(image, model):
     results = model(img)
     boxes = results[0].boxes.xyxy.cpu().tolist()
     classes = results[0].boxes.cls.cpu().tolist()
+
+    cropped_images = []
     
     for box, cls in zip(boxes, classes):
         # Get the class name from the model class dictionary
@@ -40,9 +42,11 @@ def crop_image(image, model):
         if class_name == 'person':
             # Crop the image
             crop_img = image.crop((box[0], box[1], box[2], box[3]))
-            return crop_img
+            cropped_images.append(crop_img)
         else:
-            return None
+            print(f"Skipping {class_name}")
+            continue
+    return cropped_images if cropped_images else None
 
 
 def crop_and_save_images(source_dir, model_path='yolov10x.pt'):
@@ -61,7 +65,7 @@ def crop_and_save_images(source_dir, model_path='yolov10x.pt'):
     files = os.listdir(source_dir)
     for file in files:
         # Check if the file is a jpg
-        if file.endswith('.jpg'):
+        if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.png'):
             name, _ = os.path.splitext(file)
             image_path = os.path.join(source_dir, file)
             print(f"Processing {file}")
@@ -70,16 +74,23 @@ def crop_and_save_images(source_dir, model_path='yolov10x.pt'):
             image = Image.open(image_path)
 
             # Crop the image
-            crop_img = crop_image(image, model)
+            cropped_imgs = crop_image(image, model)
+
+            if len(cropped_imgs) == 0:
+                print("No humans detected.")
+                continue
 
             # Save the cropped image
-            crop_img.save(os.path.join(source_dir, f"{name}_person.jpg"))
-            
+            else:
+                for i, crop_img in enumerate(cropped_imgs):
+                    crop_img.save(os.path.join(source_dir, f"{name}_{i}_person.jpg"))
+                    
             print("Cropping and saving complete.")
 
 if __name__ == "__main__":
 
     source_dir = find_folder('data/models', 'VL00562')
+    source_dir = 'data/processed'
     print(f"Found folder: {source_dir}")
     
     crop_and_save_images(source_dir)
